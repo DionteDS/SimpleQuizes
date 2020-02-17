ed963033021a55a7719ed47c077f2befd956cfcb
Dionte <Dionte_sb@yahoo.com>
2020-02-17 10:48:58 -0800

    user can type in their answer. Check button will check if the answer is correct or wrong

--- SimpleQuizes/Controllers/SpaceViewController.swift
+++ SimpleQuizes/Controllers/SpaceViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class SpaceViewController: UIViewController, UITextFieldDelegate {
+class SpaceViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var progressBar: UIView!
     
     private var allQuestions = SpaceQuestionBank()
+    private var questionNumber = 0
+    private var score = 0
+    private var userAnswer = ""
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        questionLabel.text = allQuestions.list.first?.textQuestion
+        
     }
     
     @IBAction func checkAnswer(_ sender: UIButton) {
         
+        checkAnswer()
+        
+    }
+    
+    private func checkAnswer() {
+        
+        let currentAnswer = allQuestions.list[questionNumber].answer
+        
+        if userAnswer == currentAnswer || userAnswer == currentAnswer.lowercased() {
+            print("Correct")
+        } else {
+            print("Wrong!")
+        }
     }
     
 
+}
+
+extension SpaceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        userAnswer = textField.text!
+        
+    }
+    
 }
