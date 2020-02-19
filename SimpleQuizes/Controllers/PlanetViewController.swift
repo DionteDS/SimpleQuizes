0d555de6fe56aa471d24d6b232567e1d14c7dbc6
Dionte <Dionte_sb@yahoo.com>
2020-02-19 13:32:18 -0800

    User answer gets checked and then move on to the next question

--- SimpleQuizes/Controllers/PlanetViewController.swift
+++ SimpleQuizes/Controllers/PlanetViewController.swift
@@ -30,15 +30,55 @@ class PlanetViewController: UIViewController {
         navigationItem.title = "Planet Quiz"
         navigationItem.largeTitleDisplayMode = .always
         
+        nextQuestion()
     }
     
     
+    //MARK: - Check Answer Button
     
     @IBAction func checkAnswerDidTapped(_ sender: UIButton) {
         
         sender.pulse()
+        
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
+    }
+    
+    // Go the next question
+    func nextQuestion() {
+        
+        if questionNumber <= 4 {
+            questionLabel.text = allQuestions.list[questionNumber].textQuestion
+        }
+        
     }
     
     
+    // Check if the answer is right or wrong
+    func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if userAnswer == correctAnswer || userAnswer == correctAnswer.lowercased() || userAnswer == correctAnswer.uppercased() {
+            print("correct!")
+        } else {
+            print("Wrong!")
+        }
+        
+    }
 
+}
+
+extension PlanetViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        userAnswer = textField.text!
+    }
+    
 }
